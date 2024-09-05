4ffd1f006e7599f4b4b178273817753f2c424990
Iceflame07 <Chibizz007@gmail.com>
2024-09-05 09:25:11 +0100

    my third commit

---
+++ pythonscore.py
@@ -0,0 +1,12 @@
+'''
+Prompt the user to enter a score and save
+Prompt the user to enter you passed if the score is greater than 80 and save
+Prompt the user to enter you are a failure you can never make it if the score is less than 80 and save
+'''
+
+number = int(input("Enter a number: "))
+if(number > 80):
+  print("you passed")
+elif:
+  print("you are a failure you can never make it")
+
