15186f8c6d30c73b6eafa4c7cd3378b9e7b8631f
Iceflame07 <Chibizz007@gmail.com>
2024-09-05 09:24:02 +0100

    my second commit

---
+++ creditcalculator.py
@@ -0,0 +1,21 @@
+'''
+Prompt the user for the account number and save
+Prompt the user for the first balance of the month and save
+Prompt the user for the items charged for the month and save
+Prompt the user for the credit applied to the customer's account and save
+Prompt the user for the credit limit and save
+'''
+
+account = int(input("Enter account number:$ "))
+
+firstbalance = int(input("Enter first balance of the month:$ "))
+
+items = int(input("Enter items charged for the month:$ "))
+
+creditapplied = int(input("Enter credit applied to the customer's account:$ "))
+
+limit = int(input("Enter credit limit applied to the customer's account:$ "))
+
+account = (account_+_creditlimit_-_firstbalance)
+
+newbalance = int(input("Enter the new balance:$ "))
